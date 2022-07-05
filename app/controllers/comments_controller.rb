feb934f8fcd0684708b3b38d185759910bb85166
Nikita Stokalyuk <zippo1618@gmail.com>
2022-07-05 22:05:39 +0300

    single delete comment DONE

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,17 +2,24 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
-    redirect_to post_path(@post)
-  end
 
-  def destroy
-    @comment = Comment.find(params[:id])
-    @comment.destroy
-    respond_to do |format|
-      format.html{ redirect_to post_path}
+    if @comment.save
+      flash[:success] = 'Комментарий создан'
+      redirect_to post_url(@post)
     end
   end
 
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+
+    respond_to do |format|
+      format.html { redirect_to @post, notice: "Комментарий удален" }
+      format.json { head :no_content }
+    end
+
+  end
 
   private def comment_params
     params.require(:comment).permit(:username, :content)
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,7 +28,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to post_url(@post), notice: "Создан новый пост" }
+        format.html { redirect_to post_url(@post), notice: 'Создан новый пост' }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -43,7 +43,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to post_url(@post), notice: "Пост был обновлён" }
+        format.html { redirect_to post_url(@post), notice: 'Пост был обновлён' }
         format.json { render :show, status: :ok, location: @post }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -56,7 +56,7 @@ class PostsController < ApplicationController
   def destroy
     @post.destroy
     respond_to do |format|
-      format.html { redirect_to posts_url(@post), notice: "Пост удалён" }
+      format.html { redirect_to posts_url(@post), notice: 'Пост удалён' }
       format.json { head :no_content }
     end
   end
