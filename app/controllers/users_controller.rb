cfa8e8b1e58bfdd8e3bde634fcd6825180692cdb
Stokalyuk Nikita Alexeevich <86319015+zippo761@users.noreply.github.com>
2022-07-29 12:51:23 +0300

    Delete method set_grants
    Not use anymore

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_user, only: %i[destroy set_grants]
+  before_action :set_user, only: %i[destroy]
   load_and_authorize_resource
 
   def index
